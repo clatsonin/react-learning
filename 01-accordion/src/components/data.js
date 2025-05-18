d9f5a2d6f0cae0187ba9934fa6a9a641c708d888
clatsonin <clatsonin@gmail.com>
2025-05-18 19:38:07 +0530

    accordion project started

---
+++ 01-accordion/src/components/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: '1',
+    question: 'What is React?',
+    answer: 'React is a JavaScript library for building user interfaces, maintained by Meta.'
+  },
+  {
+    id: '2',
+    question: 'What is a component in React?',
+    answer: 'A component is a reusable piece of UI that can be a function or a class.'
+  },
+  {
+    id: '3',
+    question: 'What is a prop in React?',
+    answer: 'Props are inputs to components. They are passed via HTML attributes and help customize the component.'
+  },
+  {
+    id: '4',
+    question: 'What is state in React?',
+    answer: 'State is a built-in object that stores property values that belong to the component and determines how it behaves.'
+  }
+];
+
+export default data;
