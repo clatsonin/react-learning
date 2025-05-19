deee8fc9d929b8e20c6c99357136fad93e752732
clatsonin <clatsonin@gmail.com>
2025-05-19 13:27:17 +0530

    SingleSelection Added

--- 01-accordion/src/App.jsx
+++ 01-accordion/src/App.jsx
@@ -5,6 +5,10 @@ import './App.css';
 function App() {
   const [selected, setSelected] = useState(null);
 
+  function handleSingleSelection(getCurrentId) {
+    
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
   return (
     <>
       <div className="wrapper">
@@ -12,10 +16,15 @@ function App() {
           {data && data.length > 0 ? (
             data.map(dataItem => (
               <div className="item" key={dataItem.id}>
-                <div className="title">
+                <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
                   <h3>{dataItem.question}</h3>
                   <span>+</span>
                 </div>
+                {
+                  selected === dataItem.id ?
+                    <div className="content">{dataItem.answer}</div>
+                    : null
+                }
               </div>
             ))
           ) : (
